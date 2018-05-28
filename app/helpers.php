ae51d5bcbc67ca8e4d821a13133188630d48bf14
alexey-ringo <alexey.ringo@gmail.com>
2018-05-28 10:00:27 +0000

    Category-set Active category

--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,9 @@ function presentPrice($price)
 {
     return money_format('$%i', $price / 100);
 }
+
+//Установка  css-класса активной категории на странице магазина для выбранной категории
+function setActiveCategory($category, $output = 'active')
+{
+    return request()->category == $category ? $output : '';
+}
