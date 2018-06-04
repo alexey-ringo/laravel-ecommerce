49770fd2b9e2ce887e058702996dbff0fbeb14b6
alexey-ringo <alexey.ringo@gmail.com>
2018-06-04 20:32:32 +0000

    Add single image by Voyager

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,9 @@ function setActiveCategory($category, $output = 'active')
 {
     return request()->category == $category ? $output : '';
 }
+
+function productImage($path) {
+    return $path && file_exists('storage/' . $path) ? asset('storage/' . $path) : asset('img/not-found.jpg');
+    
+    
+}
